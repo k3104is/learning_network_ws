5036a50fc3b01945353b706b794c13b79c07c174
Satoshi Kataoka <k.3104is@gmail.com>
2022-10-07 21:54:54 +0900

    edited tls.sh

--- transport_layer/tls.sh
+++ transport_layer/tls.sh
@@ -1,41 +1,30 @@
 #!/bin/bash
+# generate a private key for a curve
+openssl ecparam -name prime256v1 -genkey -noout -out private-key.pem
 
-# make self certification
-#openssl req -new -x509 -keyout test-key.pem -out test-cert.pem
+# create a self-signed certificate
 openssl req -new -x509 \
-  -keyout test-key.pem \
-  -out test-cert.pem \
-  -passout pass:test \
+  -key private-key.pem \
+  -out cert.pem \
   -subj "/C=JP/ST=Nagoya/O=myhome/CN=localhost"
 sleep 1
 
-# launch tcp server
-#echo "test" | ncat -lnv 127.0.0.1 54321 --ssl --ssl-cert test-cert.pem --ssl-key test-key.pem  > /dev/null 2>&1 &
-
-expect -c "
-  set timeout 3
-  spawn ncat -lnv 127.0.0.1 54321 --ssl --ssl-cert test-cert.pem --ssl-key test-key.pem
-  expect \"Enter PEM pass phrase:\"
-  send \"test\n\"
-  interact
-"
+# launch server
+sudo openssl s_server -accept 54321 -cert cert.pem -key private-key.pem &
 sleep 1
 
 # tcpdump to check communication
 sudo tcpdump -i lo -tnlA "tcp and port 54321" -w - > tls.cap &
 sleep 1
 
-# launch udp client
-echo "Hello, World!" | ncat 127.0.0.1 54321 --ssl-verify --ssl-trustfile test-cert.pem &
+# launch client
+echo "Hello, World!" | sudo openssl s_client -connect 127.0.0.1:54321 -CAfile cert.pem
 sleep 1
 
 # delete task
 kill %1 > /dev/null 2>&1
-kill %3 > /dev/null 2>&1
-jobs > /dev/null 2>&1
-jobs -l | awk -F' ' '{print $2}' | xargs sudo kill > /dev/null 2>&1
+kill %2 > /dev/null 2>&1
 sleep 1
-jobs  > /dev/null 2>&1
 
 #delete self certification
-sudo rm -rf ./test-key.pem ./test-cert.pem
+sudo rm -rf ./private-key.pem ./cert.pem
